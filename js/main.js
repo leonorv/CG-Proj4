acf3ef60d0153688e58f4c8ab21491581973c114
leonorv <leonor.veloso@tecnico.ulisboa.pt>
2020-11-20 15:20:58 +0000

    first commit

---
+++ js/main.js
@@ -0,0 +1,97 @@
+var camera, scene, renderer;
+
+var SCREEN_WIDTH = window.innerWidth;
+var SCREEN_HEIGHT = window.innerHeight;
+var aspect = SCREEN_WIDTH / SCREEN_HEIGHT;
+var cameraFront, cameraTop, cameraSide;
+var frustumSize = 220;
+var un = 5;
+
+var keys = {
+}
+
+
+
+function createScene() {
+    'use strict';
+
+    scene = new THREE.Scene();
+    scene.add(new THREE.AxisHelper(10));
+
+    scene.add(new THREE.AmbientLight(0x404040)); //soft ambient light
+
+}
+
+function createCamera() {
+    'use strict';
+
+}
+
+function onResize() {
+    'use strict';
+
+    SCREEN_WIDTH = window.innerWidth;
+	SCREEN_HEIGHT = window.innerHeight;
+    aspect = SCREEN_WIDTH / SCREEN_HEIGHT;
+
+    renderer.setSize( SCREEN_WIDTH, SCREEN_HEIGHT );
+
+    camera.left = - 0.5 * frustumSize * aspect / 2;
+    camera.right = 0.5 * frustumSize * aspect / 2;
+    camera.top = 0.5 * frustumSize / 2;
+    camera.bottom = - 0.5 *  frustumSize / 2;
+    camera.updateProjectionMatrix();
+}
+
+function onKeyDown(e) {
+    'use strict';
+    keys[e.keyCode] = true;
+
+    switch(e.keyCode) {
+
+    }
+}
+
+function onKeyUp(e) {
+    'use strict';
+    keys[e.keyCode] = false;
+}
+
+function render() {
+    'use strict';
+    delta = clock.getDelta();
+    keyPressed(delta);
+    renderer.render(scene, camera);
+}
+
+function keyPressed(delta) {
+
+}
+
+function init() {
+    'use strict';
+    renderer = new THREE.WebGLRenderer({
+        antialias: true
+    });
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    clock = new THREE.Clock();
+    clock.start();
+
+    createScene();
+    createCamera();
+
+    render();
+
+    window.addEventListener("keydown", onKeyDown);
+    window.addEventListener("keyup", onKeyUp);
+    window.addEventListener("resize", onResize);
+}
+
+function animate() {
+    'use strict';
+    render();
+    requestAnimationFrame(animate);
+}
